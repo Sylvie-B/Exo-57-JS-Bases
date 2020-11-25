efe93f6cddf3f00f0838f49482cb6d85f9b9a901
Sylvie B <unsolo@hotmail.fr>
2020-11-25 16:31:49 +0100

    exo 5 sans les parties décimales

--- exo 5/script-exo57-5.js
+++ exo 5/script-exo57-5.js
@@ -1,6 +1,8 @@
 let txt01 = prompt("entrez un nombre à virgule");
+console.log(txt01);
 
 let txt02 = prompt("entrez un deuxième nombre à virgule");
+console.log(txt02);
 
 let nbr1 = parseInt(txt01);
 console.log(nbr1);
@@ -9,6 +11,6 @@ let nbr2 = parseInt(txt02);
 console.log(nbr2);
 
 let result = nbr1 + nbr2;
+console.log(result)
 
 alert(result);
-
