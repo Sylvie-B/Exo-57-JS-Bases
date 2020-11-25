20d07af85bef2432ec67c142a860224f19025a70
Sylvie B <unsolo@hotmail.fr>
2020-11-25 16:14:14 +0100

    premier essai avec parseInt

---
+++ exo 5/script-exo57-5.js
@@ -0,0 +1,14 @@
+let txt01 = prompt("entrez un nombre à virgule");
+
+let txt02 = prompt("entrez un deuxième nombre à virgule");
+
+let nbr1 = parseInt(txt01);
+console.log(nbr1);
+
+let nbr2 = parseInt(txt02);
+console.log(nbr2);
+
+let result = nbr1 + nbr2;
+
+alert(result);
+
